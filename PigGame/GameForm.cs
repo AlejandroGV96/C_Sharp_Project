088bd55328b8b8e3a9b6887bc8482311a3b61cc1
Alejandro <alejandrogarciav96@gmail.com>
2021-06-22 08:55:58 +0200

    Defined most of methods before starting the UI

--- PigGame/GameForm.cs
+++ PigGame/GameForm.cs
@@ -15,11 +15,15 @@ namespace PigGame
         public GameForm()
         {
             InitializeComponent();
+            GameState.Init();
         }
     }
 
     public class Player
     {
+        private static uint _prevDice1 = 0;
+        private static uint _prevDice2 = 0;
+
         private uint _ID;
         private uint _Score;
         private uint _RoundScore;
@@ -35,33 +39,66 @@ namespace PigGame
 
         private void EndTurn()
         {
-            // Set _RoundScore to zero
-
-            // Call GameState.UpdateUI()
+            // Set _RoundScore and prevDice to zero
+            _RoundScore = 0;
+            _prevDice1 = 0;
+            _prevDice2 = 0;
+            _IsActive = false;
 
             // Switch GameState.ActivePlayer and call GameState.ChangeTurn()
+            if (GameState.ActivePlayer == GameState.Players[0]) GameState.ActivePlayer = GameState.Players[1];
+            else GameState.ActivePlayer = GameState.Players[0];
+
+            GameState.ActivePlayer._IsActive = true;
+            GameState.ChangeTurn();
         }
 
         public void RollDice()
         {
             // Roll two dice
+            Random r = new Random();
+            uint dice1 = (uint)r.Next(1,7);
+            uint dice2 = (uint)r.Next(1, 7);
 
             // Check conditions
+                //Update the round score iff the rolled numbers were not both x1
+                bool conditionOne = !(dice1 != 1 && dice2 != 1);
+                //Update the round score iff there were not two sixes rolled in a row
+                bool conditionTwo = !(_prevDice1 + dice1 == 12 || _prevDice1 + dice2 == 12 || _prevDice2 + dice1 == 12 || _prevDice2 + dice2 == 12);
 
             // Add to _RoundScore if conditions met
+            if(conditionOne && conditionTwo)
+            {
+                _RoundScore += (dice1 + dice2);
+                _prevDice1 = dice1;
+                _prevDice2 = dice2;
+
+            }
             // Call EndTurn() if conditions are not met
+            else
+            {
+                EndTurn();
+            }
+
         }
         public void Hold()
         {
             // Add _RoundScore to _Score
-
+            _Score +=_RoundScore;
             // Evaluate if win to set GameState.ThereIsWinner to true and call GameState.EndGame() or
+            if (_Score >= GameState.WinningScore)
+            {
+                GameState.ThereIsWinner = true;
+                GameState.EndGame();
+            }
             // Call EndTurn()
+            else EndTurn();
         }
     }
 
     static public class GameState
     {
+        public static uint WinningScore;
         public static Player ActivePlayer;
         public static Player[] Players;
         public static bool ThereIsWinner;
@@ -73,33 +110,70 @@ namespace PigGame
                 new Player(0),
                 new Player(1)
             };
+            WinningScore = 100;
             ThereIsWinner = false;
             ActivePlayer = Players[0];
             Players[0]._IsActive = true;
-            UpdateUI();
+            ChangeTurn();
         }
 
         private static void UpdateUI()
         {
             // Evaluate if there is a winner to for updating UI accordingly, otherwise...
+            if (ThereIsWinner)
+            {
+                // Set winner UI for ActivePlayer
+            }
+            else
+            {
 
-            // Evaluate which player is active
-            
-            // Highlight ActivePlayer Panel and grey up non active
+             // Highlight ActivePlayer Panel and grey up non active
+                //for each player, check if active and execute if else to higlight or greyup PlayerUI panel
+                foreach (var player in Players)
+                {
+                    if (player._IsActive)
+                    {
+                        // Activate UI
+                    }
+                    else
+                    {
+                        // Deactivate UI
+                    }
+                }
+            }
         }
 
         public static void ChangeTurn()
         {
-            // Disable not Active Player buttons and Activate ActivePlayer Buttons
+
+         // Disable not Active Player buttons and Activate ActivePlayer Buttons
+            //for each player, check if active and execute if else to disable or enable buttons
+            foreach (var player in Players)
+            {
+                if (player._IsActive)
+                {
+                    // Activate controls
+                }
+                else
+                {
+                    // Deactivate controls
+                }
+            }
 
             // Call UpdateUI()
+            UpdateUI();
         }
 
-        private static void EndGame()
+        public static void EndGame()
         {
             // Disable both players' buttons
+            foreach (var player in Players)
+            {
+                // Disable controls
+            }
 
             // Call UpdateUI()
+            UpdateUI();
         }
 
         public static void NewGame()
@@ -112,15 +186,15 @@ namespace PigGame
         public static void ButtonRoll()
         {
             // Evaluate which player is active
-
             // Call ActivePlayer.RollDice()
+            ActivePlayer.RollDice();
         }
 
         public static void ButtonHold()
         {
             // Evaluate which player is active
-
             // Call ActivePlayer.Hold()
+            ActivePlayer.Hold();
         }
     }
 
